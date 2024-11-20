c8fc63d07fcca44561da3ecf1918b4e3197865a3
Ellie Fagerberg <hi@ellie.moe>
2024-11-20 11:07:21 +0100

    Add login page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,23 @@
 import { payloadClient } from "@/utils/payload-client";
 import { useQuery } from "@tanstack/react-query";
-import { Text, View } from "react-native";
-import { Button } from "tamagui";
+import { Link } from "expo-router";
+import { Button, YStack, Text } from "tamagui";
 
 export default function Index() {
   const { data, isPending } = useQuery({
-    queryKey: [
-      "events",
-    ],
+    queryKey: ["events"],
     queryFn: () => payloadClient.collections.event.find(),
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <YStack gap="$4" padding="$4">
       <Button onPress={() => console.log("Hello")}>Hello</Button>
       {isPending && <Text>Loading..</Text>}
-      {data && data.docs.map((event) => <Text key={event.id}>{event.title}</Text>)}
-    </View>
+      {data &&
+        data.docs.map((event) => <Text key={event.id}>{event.title}</Text>)}
+      <Link push href="/login" asChild>
+        <Button variant="outlined">To login</Button>
+      </Link>
+    </YStack>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,9 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { config } from '@tamagui/config/v3';
-import { TamaguiProvider, createTamagui } from 'tamagui';
+import { TamaguiProvider, createTamagui } from "tamagui";
+import tamaguiConfig from "@/tamagui.config";
 
 const queryClient = new QueryClient();
-const tamaguiConfig = createTamagui(config)
-
-// TypeScript types across all Tamagui APIs
-type Conf = typeof tamaguiConfig
-declare module '@tamagui/core' {
-  interface TamaguiCustomConfig extends Conf { }
-}
 
 export default function RootLayout() {
   return (
@@ -18,8 +11,9 @@ export default function RootLayout() {
       <QueryClientProvider client={queryClient}>
         <Stack>
           <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="login" />
         </Stack>
       </QueryClientProvider>
-    </TamaguiProvider >
+    </TamaguiProvider>
   );
 }
