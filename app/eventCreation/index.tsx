0f05c4e6f691d396d133b2f2af92d56bc7f06780
Vasilena Karaivanova <vasilena@student.chalmers.se>
2024-11-26 12:21:41 +0100

    #2 Android fix again

--- app/eventCreation/index.tsx
+++ app/eventCreation/index.tsx
@@ -2,15 +2,14 @@ import {
   View,
   KeyboardAvoidingView,
   Platform,
-  TouchableWithoutFeedback,
   Keyboard,
   Pressable,
 } from "react-native";
-import { Button, Label, YStack, SizableText, TextArea, Text, XStack } from "tamagui";
+import { Button, YStack, SizableText, TextArea, XStack, ScrollView } from "tamagui";
 import React, { useState } from "react";
 import { FormField } from "@/components/FormField";
 import { useForm } from "react-hook-form";
-import { Link, router } from "expo-router";
+import { router } from "expo-router";
 import { useEventCreation } from "@/hooks/use-event-creation";
 import { useUser } from "@/hooks/use-user";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
@@ -68,111 +67,107 @@ export default function EventCreation() {
     setTimePickerVisibility(false);
   };
 
-  const handleConfirm = (date: any) => {
-    hideDatePicker();
-    hideTimePicker();
-    form.setValue("eventDate", date); //passing the full UTC date to the form
-  };
-
   return (
     <KeyboardAvoidingView
       keyboardVerticalOffset={Platform.select({ ios: 0, android: 500 })}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1, backgroundColor: "white" }}
     >
-      <Pressable onPress={Keyboard.dismiss}>
-        <YStack padding="$4" gap="$2">
-          <SizableText size="$9" fontSize={32} fontWeight={"bold"}>
-            Create an event
-          </SizableText>
-          <View>
-            <FormField
-              form={form}
-              name="title"
-              label="Event title"
-              inputProps={{
-                placeholder: "eg. Bingo night",
-              }}
-            />
-            <FormField
-              form={form}
-              name="address"
-              label="Event address"
-              inputProps={{
-                placeholder: "eg. Kungsportsplatsen 1",
-              }}
-            />
+      <ScrollView>
+        <Pressable onPress={Keyboard.dismiss}>
+          <YStack padding="$4" gap="$2">
+            <SizableText size="$9" fontSize={32} fontWeight={"bold"}>
+              Create an event
+            </SizableText>
             <View>
-              <View style={{ display: "flex", flexDirection: "row" }}>
-                <View style={{ marginRight: 30 }}>
-                  <FormField
-                    form={form}
-                    name="eventDate"
-                    label="Date & Time"
-                    render={({ field, inputProps }) => {
-                      const handleConfirm = (date: any) => {
-                        hideDatePicker();
-                        hideTimePicker();
-                        field.onChange(date); //passing the full UTC date to the form
-                      };
-                      return (
-                        <XStack gap="$4">
-                          <Button
-                            {...inputProps}
-                            variant="outlined"
-                            onPress={showDatePicker}
-                          >
-                            {field.value.toDateString()}
-                          </Button>
-                          <DateTimePickerModal
-                            isVisible={isDatePickerVisible}
-                            mode="date"
-                            onConfirm={handleConfirm}
-                            onCancel={hideDatePicker}
-                            minimumDate={new Date()}
-                          />
-                          <Button
-                            {...inputProps} variant="outlined" onPress={showTimePicker}>
-                            {field.value.toLocaleTimeString()}
-                          </Button>
-                          <DateTimePickerModal
-                            isVisible={isTimePickerVisible}
-                            mode="time"
-                            onConfirm={handleConfirm}
-                            onCancel={hideTimePicker}
-                            minimumDate={new Date()}
-                          />
-                        </XStack>
-                      )
-                    }}
-                  />
+              <FormField
+                form={form}
+                name="title"
+                label="Event title"
+                inputProps={{
+                  placeholder: "eg. Bingo night",
+                }}
+              />
+              <FormField
+                form={form}
+                name="address"
+                label="Event address"
+                inputProps={{
+                  placeholder: "eg. Kungsportsplatsen 1",
+                }}
+              />
+              <View>
+                <View style={{ display: "flex", flexDirection: "row" }}>
+                  <View style={{ marginRight: 30 }}>
+                    <FormField
+                      form={form}
+                      name="eventDate"
+                      label="Date & Time"
+                      render={({ field, inputProps }) => {
+                        const handleConfirm = (date: any) => {
+                          hideDatePicker();
+                          hideTimePicker();
+                          field.onChange(date); //passing the full UTC date to the form
+                        };
+                        return (
+                          <XStack gap="$4">
+                            <Button
+                              {...inputProps}
+                              variant="outlined"
+                              onPress={showDatePicker}
+                            >
+                              {field.value.toDateString()}
+                            </Button>
+                            <DateTimePickerModal
+                              isVisible={isDatePickerVisible}
+                              mode="date"
+                              onConfirm={handleConfirm}
+                              onCancel={hideDatePicker}
+                              minimumDate={new Date()}
+                            />
+                            <Button
+                              {...inputProps} variant="outlined" onPress={showTimePicker}>
+                              {field.value.toLocaleTimeString()}
+                            </Button>
+                            <DateTimePickerModal
+                              isVisible={isTimePickerVisible}
+                              mode="time"
+                              onConfirm={handleConfirm}
+                              onCancel={hideTimePicker}
+                              minimumDate={new Date()}
+                            />
+                          </XStack>
+                        )
+                      }}
+                    />
+                  </View>
                 </View>
               </View>
+              <FormField
+                form={form}
+                name="description"
+                label="Event Description"
+                render={({ field, inputProps }) => (
+                  <TextArea
+                    placeholder="Event description"
+                    height={100}
+                    {...inputProps}
+                    onChangeText={field.onChange}
+                  />
+                )}
+              />
             </View>
-            <FormField
-              form={form}
-              name="description"
-              label="Event Description"
-              render={({ field, inputProps }) => (
-                <TextArea
-                  placeholder="Event description"
-                  height={100}
-                  {...inputProps}
-                  onChangeText={field.onChange}
-                />
-              )}
-            />
-          </View>
-          <Button
-            iconAfter={<ArrowRight />}
-            backgroundColor={"#282828"}
-            color={"white"}
-            onPress={onSubmit}
-          >
-            Create event
-          </Button>
-        </YStack>
-      </Pressable>
+            <Button
+              iconAfter={<ArrowRight />}
+              backgroundColor={"#282828"}
+              color={"white"}
+              onPress={onSubmit}
+            >
+              Create event
+            </Button>
+          </YStack>
+        </Pressable>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
