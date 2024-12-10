5fc1fa999dd8f61b5dc90a84bad2bc439c7aa401
Marcelo <marsant@student.chalmers.se>
2024-12-10 14:26:19 +0100

    prettier

--- app/eventPage/[id].tsx
+++ app/eventPage/[id].tsx
@@ -87,7 +87,7 @@ export default function EventPage() {
                 (attendee) =>
                   typeof attendee !== "number" && (
                     <SizableText key={attendee.id}>{attendee.name}</SizableText>
-                  )
+                  ),
               )}
             </>
           )}
